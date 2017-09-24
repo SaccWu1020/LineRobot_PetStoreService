3752affef4db3d0b1f28bf99dcfcddc94d87fda3
easter1021 <mufasa.hsu@gmail.com>
2017-09-24 18:39:06 +0800

    將 nodejs 引擎升級到 8.5.0; 新增 debug 模式; 加入多回覆事件功能

--- routes/line/events/example.js
+++ routes/line/events/example.js
@@ -1,8 +1,11 @@
-module.exports = function (main) {
-    main.on("message", {x: 1, y: 2}, function (data, req, res) {
-        console.log("=====印出來自你傳入的變數物件=====")
-        console.log(data.params) // {x: 1, y: 2}
-        console.log("=====印出 event 物件=====")
-        console.log(this); // event from webhook
-    });
+module.exports = function (line) {
+
+    line.on(line.Event.MESSAGE, {x: 1, y: 2}, function (data, req, res) {
+        return {
+          type: 'text',
+          text: 'Hi, this is a message from example.js'
+        }
+    })
 }
+
+module.exports.debug = true
